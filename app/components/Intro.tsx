cb88257c7db78b5c95426b778fa5ca8200f1672c
Jaroslaw Weber <jaroslaw.weber@gmail.com>
2023-10-26 23:52:36 +0900

    fix title font

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -7,7 +7,7 @@ const font = Righteous({
 export function Intro() {
   return (
     <div>
-      <p className={"text-3xl text-center pb-8 font-bold uppercase"+font.className}>Major System Ai</p>
+      <p className={"text-3xl text-center pb-8 font-bold uppercase "+font.className}>Major System Ai</p>
       <p className="text-sm text-center ">generate mnemonics faster using chat gpt</p>
     </div>
   );
