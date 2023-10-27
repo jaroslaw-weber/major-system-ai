ba3425800a0413c74f783fb7b1c7ed9c131edff3
Jaroslaw Weber <jaroslaw.weber@gmail.com>
2023-10-27 21:45:22 +0900

    add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,30 @@
-import type { Metadata } from 'next'
-import { Inconsolata } from 'next/font/google'
-import './globals.css'
-
+import type { Metadata } from "next";
+import { Inconsolata } from "next/font/google";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Major System Ai',
-  description: 'Generate Major System Mnemonics with Ai',
-}
+  title: "Major System Ai",
+  description: "Generate Major System Mnemonics with Ai",
+};
 const font = Inconsolata({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* this is for veryfing google search console */}
+        <meta
+          name="google-site-verification"
+          content="Mvpa7a-ly3zvf8gi292rjnY9WwHk1UrJ14sjkXDVRZw"
+        />
+      </head>
       <body className={font.className}>{children}</body>
     </html>
-  )
+  );
 }
