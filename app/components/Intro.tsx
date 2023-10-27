70c7ff1551af5348a3331338694d0cdac65abbff
Jaroslaw Weber <jaroslaw.weber@gmail.com>
2023-10-27 22:41:43 +0900

    add 'what is major system' to page

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -8,7 +8,8 @@ export function Intro() {
   return (
     <div>
       <p className={"text-3xl text-center pb-8 font-bold uppercase "+font.className}>Major System Ai</p>
-      <p className="text-sm text-center ">generate mnemonics faster using chat gpt</p>
+      <p className=" text-center ">generate mnemonics faster using chat gpt</p>
+      <p className='text-center text-xs pt-4'>what is <a className='link link-primary' href="https://en.wikipedia.org/wiki/Mnemonic_major_system">major system?</a></p>
     </div>
   );
 }
